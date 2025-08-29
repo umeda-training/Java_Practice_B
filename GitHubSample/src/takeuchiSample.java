48ae4fe1eb1faa70515ffc39a09bc11f3ab7c9c9
student <student@NGY-F02>
2025-08-29 16:03:37 +0900

    takeuchiSample作りました
    研修のためのサンプル作り

---
+++ GitHubSample/src/takeuchiSample.java
@@ -0,0 +1,14 @@
+
+public class takeuchiSample {
+
+	public static void main(String[] args) {
+		String msg = "GitHubを使ってみよう";
+		int member = 5;
+		if(member >= 5) {
+			System.out.println(msg);
+		} else {
+			System.out.println("メンバー人数が足りません");
+		}
+	}
+
+}
